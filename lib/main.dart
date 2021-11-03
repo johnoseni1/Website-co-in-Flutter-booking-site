b4afa21e59ee5a0dfacd570a8c7d27987e01805a
John Oseni <johnoseni26@gmail.com>
2021-11-03 16:55:36 +0100

    fdinfd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:book_now/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future <void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
